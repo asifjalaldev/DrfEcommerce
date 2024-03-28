871ddf0590279e07cfcedb11c9ee74bb9fbd12a6
asifjalaldev <jalalasif92211@gmail.com>
2024-03-28 22:31:00 +0500

    create viewsets for models

--- ecomProj/product/views.py
+++ ecomProj/product/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Product, Brand
+from .serializers import CategorySerializer, ProductSerializer, BrandSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+class CategoryViewSets(ModelViewSet):
+
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    def list(self, request, *args, **kwargs):
+        queryset=Category.objects.all()
+        serialized=CategorySerializer(queryset, many=True)
+        return Response(serialized.data)
+    
+
+class ProductViewSets(ModelViewSet):
+
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    def list(self, request, *args, **kwargs):
+        queryset=Product.objects.all()
+        serialized=ProductSerializer(queryset, many=True)
+        return Response(serialized.data)
+    
+
+class BrandViewSets(ModelViewSet):
+
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    def list(self, request, *args, **kwargs):
+        queryset=Brand.objects.all()
+        serialized=BrandSerializer(queryset, many=True)
+        return Response(serialized.data)
